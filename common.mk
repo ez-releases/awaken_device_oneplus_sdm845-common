102c52cc10cbf447c26f3a3c72f723818e5fc9c2
Ꮛnѵoყ-Z <146739377+ez-releases@users.noreply.github.com>
2023-12-08 17:54:51 +0530

    Remove packages

--- common.mk
+++ common.mk
@@ -349,6 +349,10 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+	RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
